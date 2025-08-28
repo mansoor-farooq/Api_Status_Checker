96bdc395cdfbb4f7d27d6b58243abe0dfabdb3c2
mansoor-turk <mansoorgaming83@gmail.com>
2025-08-28 18:30:14 +0500

    deploying web

--- src/routes/Addroute.jsx
+++ src/routes/Addroute.jsx
@@ -2,13 +2,10 @@ import { Route, Routes } from 'react-router-dom'
 import NotFoundPage from '../pages/NotFoundPage'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
-// import Signup from '../pages/Signup'
-
 import { Fragment } from 'react'
 import PrivateRoutes from './Private_route'; // adjust the path if needed
 import YoungBazerHealth from '../pages/YoungBazerHealth'
 import Youngbazerfruntend from '../pages/Youngbazerfruntend'
-import Perfect from '../pages/Perfect'
 import AddServices from '../pages/AddServices'
 
 const Addroute = () => {
@@ -19,11 +16,8 @@ const Addroute = () => {
 
                     {""}
                     <Route path='/' element={<Home />} />
-
                     <Route path='/youngBazer' element={<YoungBazerHealth />} />
-
                     <Route path='/fruntend_youngbazer' element={<Youngbazerfruntend />} />
-                    <Route path='/test' element={<Perfect />} />
                     <Route path='/add_sevice' element={<AddServices />} />
                 </Route>
                 <Route path='/login' element={<Login />} />
